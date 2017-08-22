c571d8b5bd1744af3bd93d68a71bb19574359c9c
Tim Galloway <galloway@iinet.net.au>
2017-08-22 22:05:36 +0800

    Added List and Details pages with routing extension from Home Controller

--- DefectServer/Controllers/HomeController.cs
+++ DefectServer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DefectServer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,33 @@ namespace DefectServer.Controllers
 {
     public class HomeController : Controller
     {
+        private DefectServerContext db;
+
+        public HomeController()
+        {
+            db = new DefectServerContext();
+        }
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        public ActionResult List()
+        {
+            var Model = db.Defects.ToList();
+            return View(Model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Defect DefectModel = db.Defects
+                .Where(d => d.Id == id)
+                .FirstOrDefault();
+
+            return View(DefectModel);
         }
     }
 }
